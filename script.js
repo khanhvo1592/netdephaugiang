022312b37068dd4ea45e2d0c5df1adc54142209d
Khanh Vo <khanhvo1592@gmail.com>
2024-02-29 17:53:01 +0700

    update emails link

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-document.querySelector(".submit-link").addEventListener("click", function () {
-    // alert("Cảm ơn bạn đã tham gia chương trình Nét đẹp phụ nữ Hậu Giang!");
-});
+document
+    .querySelector(".submit-link")
+    .addEventListener("click", function (event) {
+        event.preventDefault(); // Ngăn chặn hành vi mặc định của liên kết
+        const subject = encodeURIComponent("Dự thi Nét đẹp phụ nữ Hậu Giang");
+        const mailtoLink = `mailto:tts.hgtv@gmail.com?subject=${subject}`;
+        // Thiết lập href cho liên kết của bạn
+        window.location.href = mailtoLink;
+        // Hiển thị thông báo (nếu muốn)
+        // alert("Cảm ơn bạn đã tham gia chương trình Nét đẹp phụ nữ Hậu Giang!");
+    });
